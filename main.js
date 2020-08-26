cfdd6126062cfbf8672fc380d53b2a370d3ec88c
Igor Soares <igor.hibiki@gmail.com>
2020-08-26 09:11:45 +0300

    test for request URL validation

--- main.js
+++ main.js
@@ -5,60 +5,50 @@ const request = require('request');
 
 app.use(bodyParser.json());
 
-const listener = app.listen(process.env.PORT || '3000', function () {
-  console.log('Your app is listening on port ' + listener.address().port);
-});
-
 app.get('/', function (req, res) {
-  const reply = {
-    "status": "ok"
-  };
-  res.json(reply);
+    const reply = {
+        'status': 'ok'
+    };
+    res.json(reply);
 });
 
-app.post('/action-endpoint', function (req, res) {
-  const challenge = req.body.challenge; // this challenge is needed to ensure slack that our bot works
+app.post('http://igorubot.herokuapp.com/action-endpoint', function (req, res) {
+  const challenge = req.body.challenge;
+
   const reply = {
       "challenge": challenge
   };
 
   const headers = {
     'Content-type': 'application/json',
-    'Authorization': `Bearer ${process.env.TOKEN}` // Token needs to be setup on Heroku.
+    'Authorization': `Bearer ${process.env.TOKEN}`
   }
 
   // console.log(req.body.event);
 
-  if (req.body.event.subtype != 'bot_message') { // So we won't reply to ourselves...
-    request.get('https://api.coindesk.com/v1/bpi/currentprice/EUR.json', function(err, res, body) {
+  if (req.body.event.subtype != 'bot_message') { // se we won't reply to ourselves...
+    const body = {
+      'channel': req.body.event.channel,
+      'text': 'Hello there'
+    }
+
+    const options = {
+      url:   'https://slack.com/api/chat.postMessage',
+      method: 'POST',
+      headers,
+      body:  JSON.stringify(body)
+    };
+
+    request.post(options, function(err, res, body) {
       if (err) {
         console.log(err);
       }
-      else {
-        const coindesk = JSON.parse(body);
-        const rate = coindesk.bpi.EUR.rate;
-        const reply = {
-          'channel': req.body.event.channel,
-          text: `Current BTC rate: ${rate} EUR per 1 BTC`
-        }
-
-        const options = {
-          url:   'https://slack.com/api/chat.postMessage',
-          method: 'POST',
-          headers,
-          body:  JSON.stringify(reply)
-        };
-
-        console.log(body);
-
-        request.post(options, function(err, res, body) {
-          if (err) {
-            console.log(err);
-          }
-        });
-      }
-    });
+    })
   }
 
   res.json(reply);
+});
+
+const listener = app.listen(process.env.PORT || '3000', function () {
+  console.log('Your app is listening on port ' + listener.address().port);
 });
